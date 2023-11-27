9825063c489f3f61893833b0d04e3bef918a4749
Lauren Stevens <57653314+laurenstevens257@users.noreply.github.com>
2023-11-26 21:30:34 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 //this is going to be where the magic happens
+
+//import all of the libraries we will need to use (express, cors, mongoose)
+//see video for help, im need to look more into all of this obviously
+const app = express();
+
+app.use(express.json());
+
+app.use(cors());
+
+app.listen(/*choose a port*/)
