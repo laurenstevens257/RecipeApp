b829f0f02f6bf4952abca8f378b97da2241427de
Hayk Gargaloyan <haykgargaloyan@gmail.com>
2023-11-28 03:35:05 -0800

    bringing signup changes to RecipeApp repo

--- server.js
+++ server.js
@@ -1,14 +1,38 @@
 const express = require('express');
-const cors = require('cors');
+const cors = require('cors')
 const app = express();
 
 app.use(cors());
+app.use(express.json());
 
-app.use('/login', (req, res) => {
-    res.send({
-      token: 'test123'
-    });
+const userCredentials = new Map();
+
+app.post('/signup', (req, res) => {
+
+  const { username, password } = req.body;
+  console.log(username);
+  console.log(password);
+
+  if (userCredentials.has(username)) {
+      console.log('username already exists');
+      res.status(400).json({ error: 'Username already exists', success: false });
+  } else {
+      userCredentials.set(username, password);
+      console.log('username DNE');
+      res.send({ token: 'test123', success: true });
+  }
+});
+
+app.post('/login', (req, res) => {
+  const { username, password } = req.body;
+  console.log(username);
+  console.log(password)
+  res.send({
+    token: 'test321',
+    success: true
+  });
 });
 
 app.listen(8080, () => console.log('API is running on http://localhost:8080/login'));
+
 
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -3,151 +3,118 @@ import './Login.css';
 import PropTypes from 'prop-types';
 
 
-async function loginUser(credentials) {
-    return fetch('http://localhost:8080/login', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(credentials)
-    })
-      .then(data => data.json())
-}
-
-// async function SignupUser(credentials) {
-//   return fetch('http://localhost:8080/login', {
-//     method: 'POST',
-//     headers: {
-//       'Content-Type': 'application/json'
-//     },
-//     body: JSON.stringify(credentials)
-//   })
-//     .then(data => data.json())
-// }
-
-
 
 export default function Login( { setToken } ) {
-    const [usernameLogin, setLoginUserName] = useState();
-    const [passwordLogin, setLoginPassword] = useState();
-    const [usernameSignup, setSignupUserName] = useState();
-    const [passwordSignup, setSignupPassword] = useState();
+    
+    const [usernameLogin, setLoginUserName] = useState('');
+    const [passwordLogin, setLoginPassword] = useState('');
+    const [usernameSignup, setSignupUserName] = useState('');
+    const [passwordSignup, setSignupPassword] = useState('');
     const [error, setError] = useState('');
 
-    const handleSubmitLogin = async e => {
-      e.preventDefault();
-      const token = await loginUser({
-        usernameLogin,
-        passwordLogin
-      });
-      setToken(token);
+    async function loginUser() {
+        return fetch('http://localhost:8080/login', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+              username: usernameLogin,
+              password: passwordLogin
+          })
+        })
+          .then(data => data.json())
+       }
+
+    async function SignupUser() {
+        return fetch('http://localhost:8080/signup', {
+            method: 'POST',
+            headers: {
+            'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                username: usernameSignup,
+                password: passwordSignup
+            })
+        })
+            .then(data => data.json())
     }
 
-    const handleSubmitSignup = async e => {
+    const handleSubmitLogin = async e => {
         e.preventDefault();
-        const token = await loginUser({
-          usernameSignup,
-          passwordSignup
-        });
+        const token = await loginUser();
         setToken(token);
       }
-  
-    // const handleSubmitLogin = async (e) => {
-    //   e.preventDefault();
-      
-    //   try {
-    //     const response = await fetch('http://localhost:8080/login', {
-    //       method: 'POST',
-    //       headers: {
-    //         'Content-Type': 'application/json',
-    //       },
-    //       body: JSON.stringify({ usernameLogin, passwordLogin }),
-    //     });
-  
-    //     if (response.ok) {
-    //       const loginData = await response.json();
-    //       setToken(loginData.token);
-    //     } else {
-    //       setError('Invalid username or password');
-    //     }
-    //   } catch (error) {
-    //     console.error('Error during login:', error);
-    //     setError('Error during login');
-    //   }
-    // };
 
-    // const handleSubmitSignup = async (e) => {
-    //   e.preventDefault();
-    //   try {
-    //       const response = await fetch('http://localhost:8080/signup', {
-    //           method: 'POST',
-    //           headers: {
-    //               'Content-Type': 'application/json',
-    //           },
-    //           body: JSON.stringify({ usernameSignup, passwordSignup }),
-    //       });
-
-    //       if (response.ok) {
-    //           // Automatically log in the user after successful signup
-    //           const loginResponse = await fetch('http://localhost:8080/login', {
-    //               method: 'POST',
-    //               headers: {
-    //                   'Content-Type': 'application/json',
-    //               },
-    //               body: JSON.stringify({ usernameSignup, passwordSignup }),
-    //           });
-
-    //           if (loginResponse.ok) {
-    //               const loginData = await loginResponse.json();
-    //               setToken(loginData.token);
-    //           } else {
-    //               // Handle login error after signup
-    //               console.error('Login failed after signup');
-    //           }
-    //       } else {
-    //           // Handle signup error
-    //           console.error('Signup failed');
-    //       }
-    //   } catch (error) {
-    //       console.error('Error during signup:', error);
-    //   }
-    // }
+      const handleSubmitSignup = async e => {
+        e.preventDefault();
+        try {
+            const response = await SignupUser();
     
-    return(
-      <div className="login-wrapper">
-          <h1>Please Log In</h1>
-          <form onSubmit={handleSubmitLogin}>
-          <label>
-              <p>Username</p>
-              <input type="text" onChange={e => setLoginUserName(e.target.value)}/>
-          </label>
-          <label>
-              <p>Password</p>
-              <input type="password" onChange={e => setLoginPassword(e.target.value)}/>
-          </label>
-          <div>
-              <button type="submit">Submit</button>
-          </div>
-          </form>
+            if (!response.success) {
+                // Handle server errors
+                setError('Signup failed: ' + response.statusText);
+                return;
+            } else{
+                setToken(response);
+            }
+    
+            const data = await response.json();
+    
+            // Check if token exists in the response
+            if (data.token) {
+                setError(data.token);
+                setToken(response);
+            } else {
+                // Handle case where token is not provided
+                setError('Signup successful, but no token received');
+            }
+        } catch (error) {
+            // Handle network errors
+            setError('other error occurred during signup');
+        }
+    }
+    
 
-        <h1>Please Sign Up</h1>
-        <form onSubmit={handleSubmitSignup}>
-          <label>
-              <p>Username</p>
-              <input type="text" onChange={e => setSignupUserName(e.target.value)}/>
-          </label>
-          <label>
-              <p>Password</p>
-              <input type="password" onChange={e => setSignupPassword(e.target.value)}/>
-          </label>
-          <div>
-              <button type="submit">Submit</button>
-          </div>
-        </form>
-      </div>
+    return(
+        <div>
+            <div className="login-wrapper">
+                <h1>Please Log In</h1>
+                <form onSubmit={handleSubmitLogin}>
+                    <label>
+                        <p>Username</p>
+                        <input type="text" onChange={e => setLoginUserName(e.target.value)}/>
+                    </label>
+                    <label>
+                        <p>Password</p>
+                        <input type="password" onChange={e => setLoginPassword(e.target.value)}/>
+                    </label>
+                    <div>
+                        <button type="submit">Submit</button>
+                    </div>
+                </form>
+
+                <h1>Or Sign Up</h1>
+                <form onSubmit={handleSubmitSignup}>
+                    <label>
+                        <p>Username</p>
+                        <input type="text" onChange={e => setSignupUserName(e.target.value)}/>
+                    </label>
+                    <label>
+                        <p>Password</p>
+                        <input type="password" onChange={e => setSignupPassword(e.target.value)}/>
+                    </label>
+                    <div>
+                        <button type="submit">Submit</button>
+                    </div>
+                </form>
+            </div>
+            {error && <p style={{ color: 'red' }}>{error}</p>}
+        </div>
     )
 }
 
 Login.propTypes = {
     setToken: PropTypes.func.isRequired
   }
+
