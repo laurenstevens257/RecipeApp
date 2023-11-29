8bb1a7d78c30c1662172df145b93bdf38063dc9c
nmehndir <nmehndir@gmail.com>
2023-11-28 23:10:52 -0800

    e Integrate AddRecipe and RecipeList into App.js

--- client/recipe-app/src/components/App/App.js
+++ client/recipe-app/src/components/App/App.js
@@ -4,12 +4,17 @@ import Login from '../Login/Login';
 import useToken from './useToken';
 import Dashboard from '../Dashboard/Dashboard';
 import Preferences from '../Preferences/Preferences';
+import AddRecipe from '../AddRecipe/AddRecipe'; // Adjust the path as needed
+import RecipeList from '../RecipeList/RecipeList'; // Adjust the path as needed
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-
 function App() {
-
   const { token, setToken } = useToken();
+  const [recipes, setRecipes] = useState([]); // State to store recipes
+
+  const handleAddRecipe = (recipeName) => {
+    setRecipes([recipeName, ...recipes]); // Adds new recipe to the top of the list
+  };
 
   if(!token) {
     return <Login setToken={setToken} />
@@ -22,6 +27,8 @@ function App() {
         <Routes>
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/preferences" element={<Preferences />} />
+          <Route path="/add-recipe" element={<AddRecipe onAdd={handleAddRecipe} />} />
+          <Route path="/recipes" element={<RecipeList recipes={recipes} />} />
         </Routes>
       </BrowserRouter>
     </div>
