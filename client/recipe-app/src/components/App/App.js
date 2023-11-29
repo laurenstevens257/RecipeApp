4349b1bb9ae2f6c6853735db247f82cac0210a88
Hayk Gargaloyan <haykgargaloyan@gmail.com>
2023-11-28 20:51:16 -0800

    finished merge with UserAuth

--- client/recipe-app/src/components/App/App.js
+++ client/recipe-app/src/components/App/App.js
@@ -7,16 +7,6 @@ import Preferences from '../Preferences/Preferences';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 
-// function setToken(userToken) {
-//   sessionStorage.setItem('token', JSON.stringify(userToken));
-// }
-
-// function getToken() {
-//   const tokenString = sessionStorage.getItem('token');
-//   const userToken = JSON.parse(tokenString);
-//   return userToken?.token
-// }
-
 function App() {
 
   const { token, setToken } = useToken();
