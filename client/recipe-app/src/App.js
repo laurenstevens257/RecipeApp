a79db831f6fc132edb5a676e7a921e362f8696f5
Mashamell0w <masha.koldubaeva@gmail.com>
2023-11-28 17:01:33 -0800

    Add PersonalPage component and update App.js

--- client/recipe-app/src/App.js
+++ client/recipe-app/src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import PersonalPage from './PersonalPage'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/*<header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +18,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header>*/}
     </div>
   );
 }
