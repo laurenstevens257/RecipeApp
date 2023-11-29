242fa9199b5e84e5c37accfc31996cfc05c947c9
nmehndir <nmehndir@gmail.com>
2023-11-28 23:36:49 -0800

    Updated AddRecipe and RecipeList so they talk to the server

--- client/recipe-app/src/components/App/RecipeList.js
+++ client/recipe-app/src/components/App/RecipeList.js
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-function RecipeList({ recipes }) {
-  // Apply the 'recipe-list' and 'recipe-item' CSS classes
+function RecipeList() {
+  const [recipes, setRecipes] = useState([]);
+
+  useEffect(() => {
+    const fetchRecipes = async () => {
+      try {
+        const response = await fetch('/api/recipes'); // Replace with your API endpoint
+        if (response.ok) {
+          const recipesData = await response.json();
+          setRecipes(recipesData);
+        } else {
+          // Handle errors
+          console.error('Failed to fetch recipes');
+        }
+      } catch (error) {
+        console.error('Error:', error);
+      }
+    };
+
+    fetchRecipes();
+  }, []);
+
   return (
     <div className="recipe-list">
       {recipes.map((recipe, index) => (
         <div key={index} className="recipe-item">
-          {recipe}
+          {recipe.name} {/* Assuming recipe object has a 'name' property */}
         </div>
       ))}
     </div>
