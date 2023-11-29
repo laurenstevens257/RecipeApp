eee8dd20622c943900a471826ec4266ae0fdfacc
LaurenStevens257 <laurenstevens257@gmail.com>
2023-11-29 13:17:32 -0800

    housekeeping! npm install mongodb

--- server/server.js
+++ server/server.js
@@ -1,4 +1,4 @@
-const { MongoClient, ObjectID } = require('mongodb');   //added this for mongodb
+const MongoClient = require('mongodb').MongoClient;
 const express = require('express');
 const cors = require('cors')
 const app = express();
