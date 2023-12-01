dc4be1fba4302bf30cb3f0202618a54cf057fde8
sdetorres2024 <saradetorres24@gmail.com>
2023-12-01 02:13:51 -0800

    cleaning

--- client/recipe-app/src/components/App/HomeDisplay.js
+++ client/recipe-app/src/components/App/HomeDisplay.js
@@ -6,10 +6,8 @@ import { useNavigate } from 'react-router-dom';
 const HomeDisplay = () => {
   const recipes = [];
 
-  //const [featureAvailable, setFeatureAvailable] = useState(true); 
   const navigate = useNavigate();
   const handleClick = () => {
-    //setFeatureAvailable(false);
     navigate('/add-recipe'); // Navigate to /add-recipe route
   };
 
@@ -24,29 +22,3 @@ const HomeDisplay = () => {
 };
 
 export default HomeDisplay;
-
-/* 
-import { useNavigate } from 'react-router-dom';
-
-const CreatePage = () => {
-  const [featureAvailable, setFeatureAvailable] = useState(true);
-  const navigate = useNavigate(); 
-
-  const handleClick = () => {
-    setFeatureAvailable(false);
-    navigate('/add-recipe'); // Navigate to /add-recipe route
-  };
-
-  return (
-    <div>
-      {featureAvailable ? (
-        <button onClick={handleClick}>
-          <span>+ Create New Recipe</span>
-        </button>
-      ) : (
-        <p>Feature coming soon</p>
-      )}
-    </div>
-  );
-};
-*/
