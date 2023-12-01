43fb4afee735d467e0f9fbecadc47ca915430cc9
LaurenStevens257 <laurenstevens257@gmail.com>
2023-12-01 14:16:05 -0800

    flavorites page now works -still is empty tho

--- client/recipe-app/src/components/App/FlavoritesPage.js
+++ client/recipe-app/src/components/App/FlavoritesPage.js
@@ -1,25 +1,14 @@
-import React, { useState } from 'react';
-//import './FlavoritesPage.css'; // Create and import your CSS file for styling
-/*
+// FlavoritesPage.js
+
+import React from 'react';
+import './FlavoritesPage.css';
+
 const FlavoritesPage = () => {
-  const [isFavorited, setIsFavorited] = useState(false);
-
-  const toggleHeart = () => {
-    setIsFavorited(!isFavorited);
-  };
-
   return (
-    <div className="heart-icon-container">
-      <i 
-        className={`fa-heart ${isFavorited ? 'fas' : 'far'}`} 
-        style={{ color: isFavorited ? 'red' : 'black', cursor: 'pointer' }}
-        onClick={toggleHeart}
-      />
+    <div>
+      <p>This is where liked recipes will go</p>
     </div>
   );
 };
 
 export default FlavoritesPage;
-*/   
-
-//THIS IS NOT WORKING, ITS OK THO FIX LATER
