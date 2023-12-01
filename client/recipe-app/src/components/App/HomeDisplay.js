6c86d0cdb8bb5ae5bab69c13500f5bb3fce8343d
sdetorres2024 <saradetorres24@gmail.com>
2023-12-01 02:02:32 -0800

    Fixed create link - deleted redundancy

--- client/recipe-app/src/components/App/HomeDisplay.js
+++ client/recipe-app/src/components/App/HomeDisplay.js
@@ -1,19 +1,44 @@
 import RecipeList from './RecipeList';
 import './HomeDisplay.css';
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const HomeDisplay = () => {
   const recipes = [];
 
-  const [featureAvailable, setFeatureAvailable] = useState(true);
-
+  //const [featureAvailable, setFeatureAvailable] = useState(true); 
+  const navigate = useNavigate();
   const handleClick = () => {
-    setFeatureAvailable(false);
+    //setFeatureAvailable(false);
+    navigate('/add-recipe'); // Navigate to /add-recipe route
   };
 
   return (
     <div>
       <RecipeList recipes={recipes} />
+        <button onClick={handleClick}>
+          <span>+ Create New Recipe</span>
+        </button>
+    </div>
+  );
+};
+
+export default HomeDisplay;
+
+/* 
+import { useNavigate } from 'react-router-dom';
+
+const CreatePage = () => {
+  const [featureAvailable, setFeatureAvailable] = useState(true);
+  const navigate = useNavigate(); 
+
+  const handleClick = () => {
+    setFeatureAvailable(false);
+    navigate('/add-recipe'); // Navigate to /add-recipe route
+  };
+
+  return (
+    <div>
       {featureAvailable ? (
         <button onClick={handleClick}>
           <span>+ Create New Recipe</span>
@@ -24,5 +49,4 @@ const HomeDisplay = () => {
     </div>
   );
 };
-
-export default HomeDisplay;
+*/
