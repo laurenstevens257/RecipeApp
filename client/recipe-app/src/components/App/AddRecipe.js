f3544a7a10cf851bb23afb923a0aff906833e752
LaurenStevens257 <laurenstevens257@gmail.com>
2023-12-01 18:39:40 -0800

    progress on ingredients list

--- client/recipe-app/src/components/App/AddRecipe.js
+++ client/recipe-app/src/components/App/AddRecipe.js
@@ -5,8 +5,24 @@ import { useNavigate } from 'react-router-dom';
 function AddRecipe({ onRecipeAdded }) { // Changed from onAdd to onRecipeAdded
   const [recipeName, setRecipeName] = useState('');
   const navigate = useNavigate();
+  const[ingredients, setIngredients] = useState([]);
+  const [ingredientInput, setIngredientInput] = useState('');
+
+  const handleAddIngredient = () => {
+    if (ingredientInput) {
+      setIngredients(prevIngredients => [...prevIngredients, ingredientInput]);
+      setIngredientInput(''); // Clear the input field
+    }
+  };
 
   async function SendRecipe(){
+
+        // Include ingredients in the data sent to the server
+        const recipeData = {
+          name: recipeName,
+          ingredients: ingredients,
+          // ... include other recipe data ...
+        };
     return (fetch('http://localhost:8080/recipe', {
       method: 'POST',
       headers: {
@@ -47,11 +63,22 @@ function AddRecipe({ onRecipeAdded }) { // Changed from onAdd to onRecipeAdded
           placeholder="Enter recipe name"
           className='title-input'
         />
+
         <input
           type="text"
           placeholder="Enter prep time in mins"
           className='prep-input'
         />
+        <input
+          type="text"
+          value={ingredientInput}
+          onChange={(e) => setIngredientInput(e.target.value)}
+          placeholder="Enter an ingredient"
+          className='ingredient-input'
+        />
+        <button type="button" onClick={handleAddIngredient} className="add-ingredient-button">+</button>
+        
+        
         <input
           type="text"
           placeholder="Enter cook time in mins"
