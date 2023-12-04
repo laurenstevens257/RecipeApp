8a63fd57e5269ac9536db208b40f45401687ec9a
LaurenStevens257 <laurenstevens257@gmail.com>
2023-12-04 13:01:37 -0800

    subtoolbar to access less important functions

--- client/recipe-app/src/components/App/Toolbar.js
+++ client/recipe-app/src/components/App/Toolbar.js
@@ -4,14 +4,22 @@ import { Link } from 'react-router-dom';
 import './Toolbar.css'; // Import the CSS file for styling
 
 const Toolbar = () => {
-  return (
-    <div className="toolbar">
-      <Link to="/home" className="toolbar-link">Home</Link>
-      <Link to="/flavorites" className="toolbar-link">Flavorites</Link>
-      <Link to="/search" className="toolbar-link">Search</Link>
-      <Link to="/calculatorPage" className="toolbar-link">Measurement Calculator</Link>
-    </div>
-  );
-};
-
-export default Toolbar;
+    return (
+      <div>
+        {/* First Toolbar */}
+        <div className="toolbar">
+          <Link to="/home" className="toolbar-link">Home</Link>
+          <Link to="/flavorites" className="toolbar-link">Flavorites</Link>
+          <Link to="/search" className="toolbar-link">Search</Link>
+        </div>
+        
+        {/* Second Toolbar */}
+        <div className="secondary-toolbar">
+          <Link to="/calculatorPage" className="toolbar-link">Measurement Calculator</Link>
+          <Link to="/groceryList" className="toolbar-link">Grocery List</Link>
+        </div>
+      </div>
+    );
+  };
+  
+  export default Toolbar;
